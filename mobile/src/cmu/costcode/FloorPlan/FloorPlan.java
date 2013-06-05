6c901362f49e17fceaa49a3d780c3d5a75b43b0a
Noh Sam Park <nohsamp@andrew.cmu.edu>
2013-06-05 18:53:01 -0400

    WiFiScanner uses FloorPlan - Before WS implements, it doesn't run FloorPlan thread

--- mobile/src/cmu/costcode/FloorPlan/FloorPlan.java
+++ mobile/src/cmu/costcode/FloorPlan/FloorPlan.java
@@ -3,7 +3,14 @@
  */
 package cmu.costcode.FloorPlan;
 
+import java.net.ConnectException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
+import org.ksoap2.serialization.SoapObject;
 
 import cmu.costcode.WIFIScanner.AccessPoint;
 
@@ -11,19 +18,67 @@ import cmu.costcode.WIFIScanner.AccessPoint;
  * @author NohSam
  * Call web service for the access point locations
  */
-public class FloorPlan {
+public class FloorPlan implements Runnable {
 	private List<AccessPoint> apList = null;
+	private WebService webService;				// Web Service Class for getting floorplan
+	private Map<String, String> wsArguments = null;	// Web Service Input parameters
 
+	/** Constructor for floorplan
+	 * 
+	 */
 	public FloorPlan() {
+		String nameSpace ="namespace";
+		String url = "url";
+		webService = new WebService(nameSpace, url);
 		
 	}
 	
-	public List<AccessPoint> getAccesspoints() {
+	/** Get Access Points information
+	 * @return List<AccessPoint>
+	 */
+	public List<AccessPoint> getAccessPoints() {
 		return apList;
 	}
 	
-	public void setAccessPoints() {
+	/** Parse results and retrieve data
+	 * 
+	 * @param soap The returned SOAP object
+	 */
+	public void getAPsFromSoap(SoapObject soap) {
+		apList = new ArrayList<AccessPoint>(soap.getPropertyCount());
 		
+		for(int i=0; i<soap.getPropertyCount(); i++) {
+			SoapObject pii = (SoapObject)soap.getProperty(i);
+			
+			// Making new AccessPoint object
+			AccessPoint ap = new AccessPoint();
+			ap.setBssid(pii.getPropertyAsString(0));
+			ap.setSsid(pii.getPropertyAsString(1));
+			ap.setPosX(Float.parseFloat(pii.getPropertyAsString(2)));
+			ap.setPosY(Float.parseFloat(pii.getPropertyAsString(3)));
+			apList.add(ap);
+		}
+	}
+
+	/** Set Web Service Inputs
+	 * 
+	 */
+	private void setWSInputs() {
+		wsArguments = new HashMap<String, String>(1);
+		wsArguments.put("input1", "value1");
 	}
 	
+	
+	
+	@Override
+	public void run() {
+		setWSInputs();
+		try {
+			getAPsFromSoap(webService.invokeMethod("methodName", wsArguments));
+		} catch (ConnectException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- mobile/src/cmu/costcode/WIFIScanner/WiFiScanner.java
+++ mobile/src/cmu/costcode/WIFIScanner/WiFiScanner.java
@@ -3,7 +3,10 @@ package cmu.costcode.WIFIScanner;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
+
+import cmu.costcode.FloorPlan.FloorPlan;
 
 import android.graphics.PointF;
 import android.net.wifi.ScanResult;
@@ -36,11 +39,23 @@ public class WiFiScanner {
 	int dropCount = 0;
 
 	private List<ScanResult> mScanResult = null; // ScanResult List
-	private List<AccessPoint> apList = new ArrayList<AccessPoint>(); // ScanResult
+	private List<AccessPoint> apList;
+	private FloorPlan fp = null;
 	
 	public WiFiScanner(WifiManager wm, int scanNumber) {
 		wifimanager = wm;
 		this.scanNumber = scanNumber;
+		
+		// Get the AP information
+		// Create thread
+		fp = new FloorPlan();
+		//fp.run(); // TODO Uncomment this line for the actual execution of web service
+		apList = fp.getAccessPoints();
+		
+		// TODO Remove the below part for the actual execution of web service
+		// mock the AP position data
+		if(apList == null)
+			apList = new ArrayList<AccessPoint>(10);
 	}
 	
 	public List<AccessPoint> scanStart() {
@@ -55,7 +70,7 @@ public class WiFiScanner {
 			wifimanager.startScan();
 			mScanResult.addAll(wifimanager.getScanResults());
 		}
-
+		
 		parseWIFIScanResult(); // get WIFISCanResult
 		mScanResult = null;
 		
@@ -66,14 +81,14 @@ public class WiFiScanner {
 		scanFlag = false;
 	}
 	
-	public void parseWIFIScanResult() {
-		Comparator<ScanResult> comparator = new Comparator<ScanResult>() {
-			@Override
-			public int compare(ScanResult s1, ScanResult s2) {
-				return (s1.BSSID.compareTo(s2.BSSID));
-			}
-		};
-		Collections.sort(mScanResult, comparator);
+	private void parseWIFIScanResult() {
+//		Comparator<ScanResult> comparator = new Comparator<ScanResult>() {
+//			@Override
+//			public int compare(ScanResult s1, ScanResult s2) {
+//				return (s1.BSSID.compareTo(s2.BSSID));
+//			}
+//		};
+//		Collections.sort(mScanResult, comparator);
 
 		int rssi = 0;
 		int tCount = 0;
@@ -82,9 +97,7 @@ public class WiFiScanner {
 		for (int i = 0; i < mScanResult.size(); i++) {
 			newResult = mScanResult.get(i);
 			if (i > 0 && oldResult.BSSID.compareTo(newResult.BSSID) != 0) {
-				// if(tCount > 0) {
 				addAP(oldResult, i, rssi / tCount);
-				// }
 				rssi = newResult.level;
 				tCount = 1;
 			} else {
@@ -98,56 +111,34 @@ public class WiFiScanner {
 		}
 	}
 		
-	public void addAP(ScanResult mResult, int i, float rssi) {
-		// mock the AP position data
-		PointF mockAP = null;
-		//
-		// points[0] = new PointF(0, 0); // siruiptime
-		// points[1] = new PointF(11.3, -2.5); // siru=softap
-		// points[2] = new PointF(15, -8.8); // siru-mac
-		// points[3]
-		if (mResult.SSID.compareTo("siruiptime") == 0) {
-			mockAP = new PointF(0, 0);
-		} else if (mResult.SSID.compareTo("siru-softap") == 0) {
-			mockAP = new PointF((float) 11.3, (float) -2.5);
-		} else if (mResult.SSID.compareTo("siru-mac") == 0) {
-			mockAP = new PointF((float) 13, (float) -10); // inside the room
-															// -10.5, outside
-															// the room -10
-		} else if (mResult.SSID.compareTo("siru-sony") == 0) {
-			mockAP = new PointF((float) 2.3, (float) -10);
-		} else {
-			mockAP = new PointF((float) i, (float) i); // 다음 라인이랑
-														// alternate comment and
-														// uncomment
-//			return;
+	private void addAP(ScanResult mResult, int i, float rssi) {
+		for (Iterator<AccessPoint> itd = apList.iterator(); itd.hasNext();) {
+			AccessPoint ap = itd.next();
+			if(ap.getSsid().equals(mResult.SSID)) {
+				ap.setBssid(mResult.BSSID);
+				ap.setCapabilities(mResult.capabilities);
+				ap.setFrequency(mResult.frequency);
+				ap.setRssi(rssi);
+			}
+			
 		}
+		
+		// TODO: delete the below part for the real WS call
+		PointF mockAP = null;
+		mockAP = new PointF((float) i, (float) i);
 		AccessPoint ap = new AccessPoint(mResult.BSSID, mResult.SSID,
 				mResult.capabilities, mResult.frequency, rssi, mockAP.x,
 				mockAP.y);
 		apList.add(ap);
 	}
 
-	public void getWIFIScanResult() {
-
-		// mScanResult = wifimanager.getScanResults(); // ScanResult
-		Comparator<ScanResult> comparator = new Comparator<ScanResult>() {
-			@Override
-			public int compare(ScanResult s1, ScanResult s2) {
-				return (s1.level >= s2.level) ? -1 : 1;
-			}
-		};
-		Collections.sort(mScanResult, comparator);
-
-		// Scan count
-		// textStatus.setText("Scan count is \t" + ++scanCount + " times \n");
-
-//		textStatus.setText("=======================================\n");
-//		for (int i = 0; i < mScanResult.size(); i++) {
-//			ScanResult result = mScanResult.get(i);
-//			textStatus.append((i + 1) + ". SSID : " + result.SSID.toString()
-//					+ "\t\t RSSI : " + result.level + " dBm\n");
-//		}
-//		textStatus.append("=======================================\n");
-	}
+//	private void getWIFIScanResult() {
+//		Comparator<ScanResult> comparator = new Comparator<ScanResult>() {
+//			@Override
+//			public int compare(ScanResult s1, ScanResult s2) {
+//				return (s1.level >= s2.level) ? -1 : 1;
+//			}
+//		};
+//		Collections.sort(mScanResult, comparator);
+//	}
 }

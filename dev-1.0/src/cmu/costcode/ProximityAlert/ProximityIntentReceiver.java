c1196b29d88d95d6a37a071423efc4feb5df31a1
Noh Sam Park <nohsamp@andrew.cmu.edu>
2013-06-07 14:24:54 -0400

    Initial dev1.0 create

---
+++ dev-1.0/src/cmu/costcode/ProximityAlert/ProximityIntentReceiver.java
@@ -0,0 +1,111 @@
+package cmu.costcode.ProximityAlert;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.location.LocationManager;
+import android.util.Log;
+import android.widget.Toast;
+import cmu.costcode.ShoppingList.R;
+import cmu.costcode.ShoppingList.LoginActivity;
+import cmu.costcode.ShoppingList.ViewListActivity;
+import cmu.costcode.ShoppingList.db.DatabaseAdaptor;
+import cmu.costcode.ShoppingList.objects.Item;
+import cmu.costcode.ShoppingList.objects.ShoppingListItem;
+
+public class ProximityIntentReceiver extends BroadcastReceiver {
+
+	private static final int NOTIFICATION_ID = 1000;
+	public static final String PROXIMITY_ALERT = "PROXIMITY_ALERT";
+	
+	private DatabaseAdaptor db;
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+		String alert = intent.getAction();
+		Boolean entering = intent.getBooleanExtra(LocationManager.KEY_PROXIMITY_ENTERING, false);
+		String category = null;
+
+		// Alert from WiFi triangulation
+		if(alert.equals(PROXIMITY_ALERT)){
+			// Get the section name (category)
+			category = intent.getStringExtra("category");
+			entering = true;
+		}
+		else {// Alert from addProximityAlert method, alert name is the category name
+			category = alert;
+		}
+		
+		// Get the message from the intent
+		int memberId = intent.getIntExtra(LoginActivity.MEMBERID, 1);
+
+		// Open database
+		db = new DatabaseAdaptor(context);
+		db.open();
+
+		Map<String, ArrayList<ShoppingListItem>> shoppingList = db.dbGetShoppingListItems(memberId);
+		String uncheckedItems = " ";
+
+		if (shoppingList != null && shoppingList.containsKey(category)) {
+
+			for (ShoppingListItem listItem : shoppingList.get(category)) {
+
+				if (!listItem.isChecked()) {
+					Item item = listItem.getItem();
+					uncheckedItems = uncheckedItems + item.getDescription();
+				}
+
+			}
+		}
+
+		if(entering) {
+			Log.d(getClass().getSimpleName(), "entering");
+			Toast.makeText(context, category + " section entering. Unchecked items: " + uncheckedItems,
+					Toast.LENGTH_LONG).show();
+
+			NotificationManager notificationManager = (NotificationManager) context
+					.getSystemService(Context.NOTIFICATION_SERVICE);
+			Notification notification = createNotification();
+
+			Intent viewIntent = new Intent(context, ViewListActivity.class);
+			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+					viewIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+			notification.setLatestEventInfo(context, category
+					+ " section entering. Unchecked items: " + uncheckedItems,
+					"You are entering your point of interest.", pendingIntent);
+			notificationManager.notify(NOTIFICATION_ID, notification);	
+		} 
+		else {
+			Log.d(getClass().getSimpleName(), "exiting");
+			Toast.makeText(context, category + " section exiting.",	Toast.LENGTH_LONG).show();
+		}
+	}
+	
+	private Notification createNotification() {
+		Notification notification = new Notification();
+
+		notification.icon = R.drawable.icon;;
+		notification.when = System.currentTimeMillis();
+
+		notification.flags |= Notification.FLAG_AUTO_CANCEL;
+		notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+
+		notification.defaults |= Notification.DEFAULT_VIBRATE;
+		notification.defaults |= Notification.DEFAULT_LIGHTS;
+
+		notification.ledARGB = Color.WHITE;
+		notification.ledOnMS = 1500;
+		notification.ledOffMS = 1500;
+
+		return notification;
+	}
+
+}
---
+++ dev-1.0/src/cmu/costcode/WIFIScanner/AccessPoint.java
@@ -0,0 +1,201 @@
+package cmu.costcode.WIFIScanner;
+
+public class AccessPoint {
+	/**
+	 * @uml.property  name="bssid"
+	 */
+	private String bssid;
+	
+	/**
+	 * @uml.property  name="ssid"
+	 */
+	private String ssid;
+	
+	/**
+	 * @uml.property  name="capabilities"
+	 */
+	private String capabilities;
+	
+	/**
+	 * @uml.property  name="frequency"
+	 */
+	private int frequency;
+	
+	/**
+	 * @uml.property  name="rssi"
+	 */
+	private float rssi;
+	
+	private float posX;
+	private float posY;
+	
+	private double distance;
+	
+	/**
+	 * @uml.property name = "Description"
+	 * Categroy description in which the AP is located such as "Food", "Clothes"
+	 */
+	private String description;
+
+	
+	public AccessPoint(String bssid, String ssid, String capabilities, int frequency, float rssi, float posX, float posY) {
+		this.bssid = bssid;
+		this.ssid = ssid;
+		this.capabilities = capabilities;
+		this.frequency = frequency;
+		this.rssi = rssi;
+		this.posX = posX;
+		this.posY = posY;
+		this.distance = 0.0F; 
+	}
+	
+	public AccessPoint() {
+		
+	}
+	
+
+	/**
+	 * @return
+	 * @uml.property  name="ssid"
+	 */
+	public String getSsid() {
+		return this.ssid;
+	}
+	
+	/**
+	 * @return
+	 * @uml.property  name="bssid"
+	 */
+	public String getBssid() {
+		return this.bssid;
+	}
+
+	/**
+	 * @return  the capabilities
+	 * @uml.property  name="capabilities"
+	 */
+	public String getCapabilities() {
+		return capabilities;
+	}
+	
+	/**
+	 * @return  the rssi
+	 * @uml.property  name="rssi"
+	 */
+	public float getRssi() {
+		return rssi;
+	}
+	
+	/**
+	 * @return  the posX
+	 * @uml.property  name="posX"
+	 */
+	public float getPosX() {
+		return posX;
+	}
+	
+	/**
+	 * @return  the posY
+	 * @uml.property  name="posY"
+	 */
+	public float getPosY() {
+		return posY;
+	}
+	
+	/**
+	 * @return  description
+	 * @uml.property  name="description"
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param capabilities  the capabilities to set
+	 * @uml.property  name="capabilities"
+	 */
+	public void setCapabilities(String capabilities) {
+		this.capabilities = capabilities;
+	}
+
+	/**
+	 * @return  the frequency
+	 * @uml.property  name="frequency"
+	 */
+	public int getFrequency() {
+		return frequency;
+	}
+	
+	/**
+	 * @return  the distance
+	 * @uml.property  name="distance"
+	 */
+	public double getDistance() {
+		return distance;
+	}
+
+	/**
+	 * @param frequency  the frequency to set
+	 * @uml.property  name="frequency"
+	 */
+	public void setFrequency(int frequency) {
+		this.frequency = frequency;
+	}
+
+		/**
+	 * @param bssid  the bssid to set
+	 * @uml.property  name="bssid"
+	 */
+	public void setBssid(String bssid) {
+		this.bssid = bssid;
+	}
+
+	/**
+	 * @param ssid  the ssid to set
+	 * @uml.property  name="ssid"
+	 */
+	public void setSsid(String ssid) {
+		this.ssid = ssid;
+	}
+	
+	/**
+	 * @param ssid  the rssi to set
+	 * @uml.property  name="rssi"
+	 */
+	public void setRssi(float rssi) {
+		this.rssi = rssi;
+	}
+	
+	/**
+	 * @param posX  the posX to set
+	 * @uml.property  name="posX"
+	 */
+	public void setPosX(float posX) {
+		this.posX = posX;
+	}
+	
+	/**
+	 * @param posY  the posY to set
+	 * @uml.property  name="posY"
+	 */
+	public void setPosY(float posY) {
+		this.posY = posY;
+	}
+	
+	/**
+	 * @param posY  the posY to set
+	 * @uml.property  name="posY"
+	 */
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+	
+	/**
+	 * @param description  category name to set
+	 * @uml.property  name="description"
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}

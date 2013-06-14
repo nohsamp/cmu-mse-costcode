62a6fc0e6ace3703950f03a370ffa7098c3c4098
Noh Sam Park <nohsamp@andrew.cmu.edu>
2013-06-14 15:26:37 -0400

    apList null check

--- mobile/src/cmu/costcode/Triangulation/WCL.java
+++ mobile/src/cmu/costcode/Triangulation/WCL.java
@@ -48,60 +48,60 @@ public class WCL extends Triangulation {
 	public AccessPoint calculateAccessPointPosition() {
 		apList = wifiScanner.scanStart();
 
-		if (apList.size() >= 3) {
-			if (triangMethod.equals("AWCL")) {
-				doAWCL();
-			}
-
-			AccessPoint nearestAP = null;
-			float sumRssi = 0;
-
-			for (Iterator<AccessPoint> it = apList.iterator(); it.hasNext();) {
-				AccessPoint dataSet = it.next();
-
-				float newRssi = (float) Math.pow(
-						Math.pow(10, dataSet.getRssi() / 20), g); // RSSI: level
-																	// of AP
-				sumRssi += newRssi;
-				dataSet.setRssi(newRssi); // calculate weight and set Rssi to
-											// weight
-				// getX, getY: location of AP --> mock data
-			}
-
-			float x = 0;
-			float y = 0;
-
-			for (Iterator<AccessPoint> itd = apList.iterator(); itd.hasNext();) {
-				AccessPoint dataSet = itd.next();
-
-				float weight = dataSet.getRssi() / sumRssi;
-				x += dataSet.getPosX() * weight;
-				y += dataSet.getPosY() * weight;
-			}
-
-			double oldDist = Double.MAX_VALUE;
-			double newDist = Double.MAX_VALUE;
-			for (Iterator<AccessPoint> itd = apList.iterator(); itd.hasNext();) {
-				AccessPoint dataSet = itd.next();
-
-				newDist = Math.sqrt((dataSet.getPosX() - x)
-						* (dataSet.getPosX() - x) + (dataSet.getPosY() - y)
-						* (dataSet.getPosY() - y));
-				if (oldDist > newDist) { // If new distance between device (x,
-											// y) and AP is shorter than old
-											// one,
-					oldDist = newDist; // Keep the shortest distance, i.e., new
-										// distance
-					nearestAP = dataSet;
-					nearestAP.setPosX(x);
-					nearestAP.setPosY(y);
-					nearestAP.setDistance(newDist);
-				}
-			}
-
-			return nearestAP;
-		} else {
+		if (apList == null || apList.size() < 3) {
 			return null;
 		}
+		
+		if (triangMethod.equals("AWCL")) {
+			doAWCL();
+		}
+
+		AccessPoint nearestAP = null;
+		float sumRssi = 0;
+
+		for (Iterator<AccessPoint> it = apList.iterator(); it.hasNext();) {
+			AccessPoint dataSet = it.next();
+
+			float newRssi = (float) Math.pow(
+					Math.pow(10, dataSet.getRssi() / 20), g); // RSSI: level
+																// of AP
+			sumRssi += newRssi;
+			dataSet.setRssi(newRssi); // calculate weight and set Rssi to
+										// weight
+			// getX, getY: location of AP --> mock data
+		}
+
+		float x = 0;
+		float y = 0;
+
+		for (Iterator<AccessPoint> itd = apList.iterator(); itd.hasNext();) {
+			AccessPoint dataSet = itd.next();
+
+			float weight = dataSet.getRssi() / sumRssi;
+			x += dataSet.getPosX() * weight;
+			y += dataSet.getPosY() * weight;
+		}
+
+		double oldDist = Double.MAX_VALUE;
+		double newDist = Double.MAX_VALUE;
+		for (Iterator<AccessPoint> itd = apList.iterator(); itd.hasNext();) {
+			AccessPoint dataSet = itd.next();
+
+			newDist = Math.sqrt((dataSet.getPosX() - x)
+					* (dataSet.getPosX() - x) + (dataSet.getPosY() - y)
+					* (dataSet.getPosY() - y));
+			if (oldDist > newDist) { // If new distance between device (x,
+										// y) and AP is shorter than old
+										// one,
+				oldDist = newDist; // Keep the shortest distance, i.e., new
+									// distance
+				nearestAP = dataSet;
+				nearestAP.setPosX(x);
+				nearestAP.setPosY(y);
+				nearestAP.setDistance(newDist);
+			}
+		}
+
+		return nearestAP;
 	}
 }
